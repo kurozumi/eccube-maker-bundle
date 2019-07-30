4f30a3de9b8b489821efdabec0784a5790bd5389
kurozumi <info@a-zumi.net>
2019-07-30 21:06:49 +0900

    update

--- Resources/skeleton/controller/Controller.tpl.php
+++ Resources/skeleton/controller/Controller.tpl.php
@@ -3,19 +3,21 @@
 namespace <?= $namespace; ?>;
 
 use Eccube\Controller\<?= $parent_class_name; ?>;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Routing\Annotation\Route;
 
 class <?= $class_name; ?> extends <?= $parent_class_name; ?><?= "\n" ?>
 {
     /**
      * @Route("<?= $route_path ?>", name="<?= $route_name ?>")
+     * @Template("<?= $template_name ?>")
      */
     public function index()
     {
 <?php if ($with_template) { ?>
-        return $this->render('<?= $template_name ?>', [
+        return [
             'controller_name' => '<?= $class_name ?>',
-        ]);
+        ];
 <?php } else { ?>
         return $this->json([
             'message' => 'Welcome to your new controller!',
